5e41ef8b975cc72e9f844e1012ea1ba2307075bb
yee-hhaa <yeehaha99@gmail.com>
2026-05-06 10:34:27 +0800

    Create 0001-Two-Sum.py
    這題使用Hash Map優化，將時間複雜度從O(n^2)降低到O(n)

---
+++ Easy/0001-Two-Sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap={}
+        for i,num in enumerate(nums):
+            complement=target-num
+            if complement in hashmap:
+                return [hashmap[complement],i]
+            hashmap[num]=i
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
