ffe23645374624fc5802953fa157b1e38029f453
yee-hhaa <yeehaha99@gmail.com>
2026-05-06 12:36:53 +0800

    Create 0013-Roman-to-Integer.py

---
+++ Easy/0013-Roman-to-Integer.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def romanToInt(self, s):
+        roman_map = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        num = 0
+        n = len(s)
+
+        for i in range(n):
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i+1]]:
+                num -= roman_map[s[i]]
+            else:
+                num += roman_map[s[i]]
+
+        return num
+        """
+        :type s: str
+        :rtype: int
+        """
+        
