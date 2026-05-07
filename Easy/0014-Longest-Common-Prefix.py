77b001fe43462f42cdc20ccf78efdd4d142a91d8
yee-hhaa <yeehaha99@gmail.com>
2026-05-07 22:35:30 +0800

    Update 0014-Longest-Common-Prefix.py

--- Easy/0014-Longest-Common-Prefix.py
+++ Easy/0014-Longest-Common-Prefix.py
@@ -1,13 +1,15 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
+        """
+        思路:垂直掃描
+        :type strs: List[str]
+        :rtype: str
+        """
         for i in range(len(strs[0])):
             char=strs[0][i]
             for j in range(1, len(strs)):
                 if i == len(strs[j]) or strs[j][i] != char:
                     return strs[0][:i]
         return strs[0]
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+
         
