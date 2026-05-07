cbc53b1d2e2999bc4c7cae89b6e8983c14c32ded
yee-hhaa <yeehaha99@gmail.com>
2026-05-07 22:32:20 +0800

    Update 0001-Two-Sum.py

--- Easy/0001-Two-Sum.py
+++ Easy/0001-Two-Sum.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        """
+        思路：Hash Map 存已遍歷的數字，O(n)
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         hashmap={}
         for i,num in enumerate(nums):
             complement=target-num
             if complement in hashmap:
                 return [hashmap[complement],i]
             hashmap[num]=i
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        
